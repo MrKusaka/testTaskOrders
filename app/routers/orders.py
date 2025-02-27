e5252ca9a420f67b77fac1868e97d93ee86335a6
MrKusaka <wow_lich2010@mail.ru>
2025-02-28 00:00:05 +0500

    fixed endpoint create order, added worker

--- app/routers/orders.py
+++ app/routers/orders.py
@@ -1,31 +1,48 @@
-from datetime import datetime
+import aio_pika
 
+from datetime import datetime
 from fastapi import APIRouter, Depends, status
 from typing import Annotated
 from sqlalchemy import insert
 from slugify import slugify
+
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.backend.db_depends import get_db
-
 from app.schemas import CreateOrder
 from app.models.orders import Orders
 
 
 router = APIRouter(prefix='/orders', tags=['order'])
 
+RABBITMQ_URL = "amqp://guest:guest@localhost/"
+QUEUE_NAME = "order_queue"
 
 
-@router.post('/create')
+async def send_to_queue(order_id: int):
+    connection = await aio_pika.connect(RABBITMQ_URL)
+    async with connection:
+        channel = await connection.channel()
+        await channel.default_exchange.publish(
+            aio_pika.Message(body=str(order_id).encode()),
+            routing_key=QUEUE_NAME,
+        )
+
+
+@router.post("/orders/")
 async def create_order(db: Annotated[AsyncSession, Depends(get_db)], create_order: CreateOrder):
-    await db.execute(insert(Orders).values(name=create_order.name,
-                                            description=create_order.description,
-                                            price=create_order.price,
-                                            status=create_order.status,
-                                            stock=create_order.stock,
-                                            created_at=datetime.now(),
-                                            slug=slugify(create_order.name)))
+    # Создаем заказ в бд и сразу возвращаем айди заказа для передачи в очередь
+    new_order = await db.execute(insert(Orders).values(name=create_order.name,
+                                                       description=create_order.description,
+                                                       price=create_order.price,
+                                                       status=create_order.status,
+                                                       quantity=create_order.quantity,
+                                                       created_at=datetime.now(),
+                                                       slug=slugify(create_order.name)).returning(Orders.id))
     await db.commit()
+
+    # Отправляем сообщение в RabbitMQ
+    await send_to_queue(new_order.one()[0])
+
     return {
         'status_code': status.HTTP_201_CREATED,
         'transaction': 'Successful'
@@ -41,12 +58,13 @@ async def get_all_orders():
 async def get_order():
     pass
 
-
-@router.put('/update_order')
-async def update_order():
-    pass
-
-
-@router.delete('/delete_order')
-async def delete_order():
-    pass
+#
+#
+# @router.put('/update_order')
+# async def update_order():
+#     pass
+#
+#
+# @router.delete('/delete_order')
+# async def delete_order():
+#     pass
---
+++ worker.py
@@ -0,0 +1,64 @@
+import asyncio
+import configparser
+
+from aio_pika import connect, IncomingMessage
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy import update
+from app.models.orders import Orders
+
+
+#
+RABBITMQ_URL = "amqp://guest:guest@localhost/"
+QUEUE_NAME = "order_queue"
+
+
+config = configparser.ConfigParser()
+config.read_file(open('C:\\Users\\wow_l\\PycharmProjects\\testTaskOrders\\config.ini'))
+
+
+engine = create_async_engine(config.get('postgres', 'db'), echo=True)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+# Функция для обновления статуса заказа
+async def update_order_status(order_id: int, new_status: str):
+    async with async_session_maker() as session:
+        async with session.begin():
+            stmt = update(Orders).where(Orders.id == order_id).values(status=new_status)
+            await session.execute(stmt)
+
+
+# Обработчик сообщений
+async def process_message(message: IncomingMessage):
+    async with message.process():
+        print(f"Заказ №{message.body.decode()} поступил на обработку")
+        # Имитация обработки заказа, приостанавливаем на n секунд
+        await asyncio.sleep(20) # 20 секунд
+        order_id = int(message.body.decode())
+
+        # Обновление статуса заказа в базе данных
+        await update_order_status(order_id, "PROCESSED")
+        async with async_session_maker() as session:
+            async with session.begin():
+                order_name = await session.get(Orders, order_id)
+
+        print(f"Статус заказа №{order_id} {order_name.name} обновлен на PROCESSED")
+
+
+# Воркер для подписки на очередь
+async def worker():
+    connection = await connect(RABBITMQ_URL)
+    async with connection:
+        channel = await connection.channel()
+        queue = await channel.declare_queue(QUEUE_NAME, durable=True)
+
+        print(f"Подписка на очередь {QUEUE_NAME}...")
+        await queue.consume(process_message)
+
+        # Бесконечный цикл для поддержания воркера
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(worker())
+
